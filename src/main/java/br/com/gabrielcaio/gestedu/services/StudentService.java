d29154931b85d5661e76071428b5ed205bc03dec
gabrielcaio11 <gabrielcaio848@gmail.com>
2025-06-15 13:05:46 -0300

    feat: add pagination and registration generation for student listing and creation

--- src/main/java/br/com/gabrielcaio/gestedu/services/StudentService.java
+++ src/main/java/br/com/gabrielcaio/gestedu/services/StudentService.java
@@ -1,5 +1,7 @@
 package br.com.gabrielcaio.gestedu.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,6 +9,7 @@ import br.com.gabrielcaio.gestedu.controllers.mapper.StudentMapper;
 import br.com.gabrielcaio.gestedu.model.student.CreateStudentDTO;
 import br.com.gabrielcaio.gestedu.model.student.Student;
 import br.com.gabrielcaio.gestedu.repositories.StudentRepository;
+import br.com.gabrielcaio.gestedu.util.StudentRegistrationGenerator;
 import br.com.gabrielcaio.gestedu.validator.ValidatorCreateStudent;
 import lombok.RequiredArgsConstructor;
 
@@ -17,21 +20,24 @@ public class StudentService {
     private final StudentRepository studentRepository;
     private final StudentMapper studentMapper;
     private final ValidatorCreateStudent validatorCreateStudent;
+    private final StudentRegistrationGenerator registrationGenerator;
 
     @Transactional(rollbackFor = Exception.class)
     public Student create(CreateStudentDTO dto) {
-
-        // MAp the DTO to the Student entity
+        // Map the DTO to the Student entity
         var student = studentMapper.toEntity(dto);
 
-        // Build the registration for the student
-        student.buildRegistration();
+        // Generate the registration for the student
+        registrationGenerator.generateRegistration(student);
 
         // Validate the student entity
         validatorCreateStudent.validate(student);
 
-        // retorn the saved student entity
+        // Return the saved student entity
         return studentRepository.save(student);
     }
 
+    public Page<Student> findAll(Pageable pageable) {
+        return studentRepository.findAll(pageable);
+    }
 }
