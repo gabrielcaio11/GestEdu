4741ddd845c8b0fc2b9c3c87e20b705f51b1735a
gabrielcaio11 <gabrielcaio848@gmail.com>
2025-06-15 12:51:34 -0300

    feat: enhance StudentMapper with null value handling and update method for DTO conversion

--- src/main/java/br/com/gabrielcaio/gestedu/controllers/mapper/StudentMapper.java
+++ src/main/java/br/com/gabrielcaio/gestedu/controllers/mapper/StudentMapper.java
@@ -1,13 +1,34 @@
 package br.com.gabrielcaio.gestedu.controllers.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
 import br.com.gabrielcaio.gestedu.model.student.CreateStudentDTO;
 import br.com.gabrielcaio.gestedu.model.student.ResponseStudentDTO;
 import br.com.gabrielcaio.gestedu.model.student.Student;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    unmappedSourcePolicy = ReportingPolicy.IGNORE
+)
 public interface StudentMapper {   
+    
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "registration", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Student toEntity(CreateStudentDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "registration", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntityFromDTO(CreateStudentDTO dto, @MappingTarget Student student);
+
     ResponseStudentDTO toDTO(Student student);
 }
